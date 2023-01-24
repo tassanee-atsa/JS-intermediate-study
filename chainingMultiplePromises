610b94edd56099c15dd3874bf15eed9d1c10e0af
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-24 21:27:40 +0000

    add files to promiseAll

--- chainingMultiplePromises
+++
@@ -1,7 +0,0 @@
-firstPromiseFunction()
-.then((firstResolveVal) => {
-  return secondPromiseFunction(firstResolveVal);
-})
-.then((secondResolveVal) => {
-  console.log(secondResolveVal);
-});
---
+++ promiseAll/app.js
@@ -0,0 +1,10 @@
+const {checkAvailability} = require('./library.js');
+
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+
+const onReject = (rejectionReason) => {
+	console.log(rejectionReason);
+};
