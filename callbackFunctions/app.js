3250ed556dc64c7149120f4a88f8efd5622aea7c
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-27 22:34:10 +0000

    add handleSuccess

--- callbackFunctions/app.js
+++ callbackFunctions/app.js
@@ -29,3 +29,24 @@ const prom = new Promise((resolve, reject) => {
   });
   
   
+  let prom = new Promise((resolve, reject) => {
+    let num = Math.random();
+    if (num < .5 ){
+      resolve('Yay!');
+    } else {
+      reject('Ohhh noooo!');
+    }
+  });
+  
+  
+  const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+  };
+  
+  
+  const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+  };
+  
+  
+  prom.then(handleSuccess, handleFailure);
