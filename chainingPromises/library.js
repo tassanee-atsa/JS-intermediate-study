ced62f7d8f2d26e8b43fd3e1df9d21110af257eb
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-27 00:00:12 +0000

    edit libbrary

--- chainingPromises/library.js
+++ chainingPromises/library.js
@@ -33,26 +33,7 @@ const store = {
    });
   };
   
-  const processPayment = (responseArray) => {
-    const order = responseArray[0];
-    const total = responseArray[1];
-    return new Promise ((resolve, reject) => {
-     setTimeout(()=> {  
-     let hasEnoughMoney = order.giftcardBalance >= total;
-     // For simplicity we've omited a lot of functionality
-     // If we were making more realistic code, we would want to update the giftcardBalance and the inventory
-     if (hasEnoughMoney) {
-       console.log(`Payment processed with giftcard. Generating shipping label.`);
-       let trackingNum = generateTrackingNumber();
-       resolve([order, trackingNum]);
-     } else {
-       reject(`Cannot process order: giftcard balance was insufficient.`);
-     }
-     
-  }, generateRandomDelay());
-   });
-  };
-  
+
   
   const shipOrder = (responseArray) => {
     const order = responseArray[0];
