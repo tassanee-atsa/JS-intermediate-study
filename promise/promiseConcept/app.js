821e995c6ebde60ccf621a85938b5e63f4159b09
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-28 22:36:47 +0000

    adding promise concept note

---
+++ promise/promiseConcept/app.js
@@ -0,0 +1,11 @@
+/*
+What is Promise ?
+Promises are objects that represent the eventual outcome of an asynchronous operation. Can be one of these 3 states.
+Pending -  initial state - operation has not completed yet
+Fulfilled - The operation has completed successfully and the promise now has its resolved value.
+Reject - The operation has failed and the promise has the reason for failure. This reason is usually an Error if some kind.
+
+Settled → fulfilled or rejected.
+
+Construction a Promise object:
+*/
