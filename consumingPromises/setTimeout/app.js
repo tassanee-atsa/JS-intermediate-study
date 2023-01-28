70da1b78a58b0fbba96da0dc4d31c7c951a93d8e
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-28 22:30:12 +0000

    add setTimeout function

--- consumingPromises/setTimeout/app.js
+++ consumingPromises/setTimeout/app.js
@@ -11,3 +11,12 @@ We invoke setTimeout () with the callback function. In at least 3 seconds delaye
 
 This delay is performed asynchronously- the rest of the program won’t stop executing during the delay. Asynchronous Javascript uses something called event-loop. After 3 secs, delayedHello() is added to a line of code waiting to be run. Before it can run, any synchronous code from the program will run.Next, any code in front of it in the line will run. This means it might be more than two seconds before delayedHello() is actually executed.
 */
+const returnPromiseFunction = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {resolve("I resolved!")}, 1000);
+  });
+  };
+  
+  const prom = returnPromiseFunction()
+  
+  
