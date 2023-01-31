63012d309013b94c5d5fccb0d381a158123f4d9f
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-31 23:58:35 +0000

    add inheritance example

--- inheritance/app.js
+++ inheritance/app.js
@@ -1,3 +1,10 @@
 //When different classes share the same properties or methods. Some classes may have additional properties. 
 //They are candidates for inheritance → a tool we use to reduce the amount of code. We create a parent class or superclass and have multiple children classes.
 
+class Cat extends Animal {
+    constructor(name, usesLitter){
+      super(name);
+      this._useLitter = usesLitter;
+     }
+}
+
