55c0bfeef0634192347f8abb9f89ed57374a8f2e
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-01-31 23:47:16 +0000

    add constructor method and create instance

--- classes/app.js
+++ classes/app.js
@@ -1,3 +1,29 @@
 //JS is an OOP 
 //Classes reduce duplicate code and debugging time.
 //constructor method & create instance 
+
+class Hello {
+    constructor(name){
+     this._name = name;
+     This._behavior = 0;}
+ 
+    get name() {
+      return this._name;
+ }
+ 
+    get behavior() {
+     return this._behavior;
+ }
+ 
+   incrementBehavior() {
+     This._behavior++ ;
+ }
+ }
+  const  helloInstance = new Hello ("tass");
+ helloInstance.incrementBehavior()
+ console.log(helloInstance.behavior) // result is 1   
+
+ //no comma between method
+ 
+ 
+ 
