77e8a6f354c01aae4c00efe06d1be08223e8c630
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2023-02-01 00:00:09 +0000

    comments

--- inheritance/app.js
+++ inheritance/app.js
@@ -13,3 +13,16 @@ class Cat extends Animal {
 
 //Static Method → can not call from the instance but can call directly from the class. Eg in Animal class —>
 
+/*static generateName(){ 
+}
+
+ animal. generateName() 
+
+constructor() is called when you create a new instance of a class. It sets the property values for each instance. If the class inherits properties from the parent class, then it will call super to pass arguments to the parent’s constructor()
+
+The subclass inherits all of the parent’s getters, setters, and methods. You can also use the super keyword to set properties in the parent class. Although the subclass inherits the parent methods, you need to use the super keyword to set the parent properties.
+
+The super keyword is used in subclasses to call a parent constructor().
+Classes made it easy to create multiple objects that share property names and methods.
+
+Purpose of inheritance is to share data and methods between objects.*/
