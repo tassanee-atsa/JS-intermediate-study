5f061fe162fc6414595c4d6b7358b1e3175bfe01
tassanee-atsa <99512238+tassanee-atsa@users.noreply.github.com>
2023-02-03 23:59:08 +0000

    Update app.js
    edit comments

--- promise/app.js
+++ promise/app.js
@@ -1,8 +1,4 @@
-/* Consuming Promises
 
-An initial state of promise is pending.  But we have a guarantee that it will settle.
-
-.then() method allows us to say. I have a promise, when it settles, then here is what I want to happen..
 
 .then() is a higher-order function that takes two callback functions as arguments → success or failure handlers (onfulfilled, onRejected).
  When the promise settles, the appropriate handler will be invoked with that settled value . We can invoke .then() with one, both or neither handler. But if the appropriate handler is not provided, instead of throwing an error,. .then() will just return a promise with the same settled value as the promise it was called on. 
